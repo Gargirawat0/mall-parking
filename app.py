5eb5ec2670345bda30042f1f04fef59778856959
Gargirawat0 <rawatgargi60@gmail.com>
2025-06-01 08:39:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -548,6 +548,3 @@ def logout():
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
